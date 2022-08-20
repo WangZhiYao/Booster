0835aac6ce7d48debed398e774eb40c93ff5ab4f
WangZhiYao <393893729@qq.com>
2022-08-21 00:26:13 +0800

    Update: change CustomerListFragment to extends BasePagingDataFragment

--- module_main/src/main/kotlin/com/yizhenwind/booster/main/ui/customer/CustomerListFragment.kt
+++ module_main/src/main/kotlin/com/yizhenwind/booster/main/ui/customer/CustomerListFragment.kt
@@ -5,9 +5,11 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.viewModels
-import com.yizhenwind.booster.component.base.BaseFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.yizhenwind.booster.common.model.Customer
+import com.yizhenwind.booster.component.base.BasePagingDataFragment
 import com.yizhenwind.booster.main.R
-import com.yizhenwind.booster.main.databinding.FragmentCustomerListBinding
 import com.yizhenwind.booster.mediator.character.ICharacterService
 import com.yizhenwind.booster.mediator.customer.ICustomerService
 import com.yizhenwind.booster.mediator.order.IOrderService
@@ -23,10 +25,9 @@ import javax.inject.Inject
  */
 @AndroidEntryPoint
 class CustomerListFragment :
-    BaseFragment<FragmentCustomerListBinding>(FragmentCustomerListBinding::inflate) {
+    BasePagingDataFragment<Customer, CustomerAdapter, CustomerViewHolder>() {
 
     private val viewModel by viewModels<CustomerListViewModel>()
-    private val adapter: CustomerAdapter by lazy { CustomerAdapter() }
 
     @Inject
     lateinit var customerService: ICustomerService
@@ -38,6 +39,7 @@ class CustomerListFragment :
     lateinit var orderService: IOrderService
 
     override fun init() {
+        super.init()
         initView()
         initData()
     }
@@ -55,16 +57,18 @@ class CustomerListFragment :
             orderService.launchCreateOrder(requireContext(), customer.id)
         }
 
-        binding.apply {
-            rvCustomer.adapter = adapter
-        }
-
         inflateMenu()
     }
 
     private fun initData() {
         viewModel.observe(viewLifecycleOwner, state = ::render)
     }
+
+    override fun getLayoutManager(): RecyclerView.LayoutManager =
+        LinearLayoutManager(requireContext())
+
+    override fun getListAdapter(): CustomerAdapter =
+        CustomerAdapter()
 
     private suspend fun render(state: CustomerListViewState) {
         when (state) {
@@ -87,4 +91,5 @@ class CustomerListFragment :
             }
         })
     }
+
 }
