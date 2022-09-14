6e94989a9e148822451ff522ac17e694bcdb4667
WangZhiYao <393893729@qq.com>
2022-09-14 23:44:52 +0800

    Update: initialize new module
    Signed-off-by: WangZhiYao <393893729@qq.com>

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+gradlePlugin {
+    plugins {
+
+    }
+}
